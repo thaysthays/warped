85b9f91c7bc7eebb64680d7082fe3a88011f5ed4
Thays <thays.giotto@escale.com.br>
2018-05-19 09:44:44 -0300

    Refactor: refactor part2

--- index.php
+++ index.php
@@ -27,7 +27,7 @@ if (isset($_POST['btnLogin'])) {
     <title>WARPR</title>
     <link rel="icon" href="/img/favicon-16x16.png" sizes="16x16">
     <link rel="icon" href="/img/favicon-32x32.png" sizes="32x32">
-    <link rel="stylesheet" href="css/main.css">
+    <link rel="stylesheet" href="dist/css/main.css">
     <script src="js/index.js"></script>
 </head>
 <body>
@@ -70,7 +70,7 @@ if (isset($_POST['btnLogin'])) {
                         <div class="msgemail"></div>
                         <input type="password" name="password1" id="senha" placeholder="Insira sua senha" maxlength="15" required="required" />
                     </div>
-                        Converse com um Freelancer disponível
+                        <div>Converse com um Freelancer disponível</div>
                         <div>
                             <button class="concluirbtn" type="submit">Concluir</button>
                         </div>
@@ -96,7 +96,7 @@ if (isset($_POST['btnLogin'])) {
                             <option value="second">Second</option>
                         </select>
                     </div>
-                    Converse com um Freelancer disponível
+                    <div>Converse com um Freelancer disponível</div>
                     <div><button class="concluirbtn" type="submit">Concluir</button></div><br>
                 </form>
                 <div class="terms">*Ao pressionar em "Concluir", você estará aceitando os <a href="#">termos e condições</a> e <a href="#">política de privacidade</a></div>
@@ -104,20 +104,25 @@ if (isset($_POST['btnLogin'])) {
         </section>
     </div>
 
-    <!-- <section>
-        <div class="textcontent">
-            <h3>Como funciona?</h3> Bacon ipsum dolor amet nisi filet mignon meatball, turkey dolore venison occaecat chicken boudin bresaola. Chicken beef rump aute eu bacon dolor pancetta ea nulla commodo kielbasa minim esse. Excepteur quis picanha, do anim shoulder ut ullamco non drumstick swine esse shank. Chuck turducken turkey eiusmod meatloaf mollit laboris lorem leberkas ham porchetta duis corned beef. Andouille eiusmod porchetta quis culpa officia laboris labore pancetta beef ribs strip steak. Cillum t-bone shankle est laboris. Sint velit drumstick kevin consequat alcatra. Dolore laboris meatball reprehenderit, dolor andouille minim sint nisi pastrami incididunt occaecat adipisicing alcatra sed. Veniam sirloin short loin aliquip shoulder do corned beef, biltong venison. Sirloin quis beef lorem chicken. Deserunt tempor anim mollit ex meatball. Pastrami andouille laborum short ribs irure, duis doner ham hock picanha flank pariatur sint in. Landjaeger cow est, in proident beef ribs aliqua doner adipisicing fugiat in pastrami capicola. Shankle jowl pastrami drumstick sirloin.Prosciutto consectetur aute voluptate officia elit pastrami reprehenderit. Drumstick turducken reprehenderit fatback, ullamco laborum in t-bone. Tail laboris anim esse. Ham venison kevin in, meatball swine do. Consequat non filet mignon sirloin ipsum ad dolore turkey ut hamburger, deserunt bacon laboris. Short loin shank adipisicing landjaeger, chuck cupidatat pastrami bacon ex ham prosciutto tenderloin strip steak picanha consectetur. Commodo exercitation ham hock landjaeger prosciutto anim do ut aliqua. Quis dolore shankle, sint occaecat beef ribs cillum aute drumstick spare ribs hamburger elit est. Pork commodo tempor porchetta proident meatball quis officia nulla sed eu beef ribs boudin adipisicing.Pariatur meatball ut cupidatat ham labore. Ham ad cupidatat, dolore in ut adipisicing eiusmod. Velit laboris excepteur nulla, sunt in ground round. Ex jerky exercitation non aliquip. Id corned beef venison, proident tempor pork tri-tip duis nostrud dolore commodo kevin frankfurter. <a href="comofunciona.php" class="login"> SAIBA MAIS ></a>
-        </div>
-    </section> -->
-
     <div class="footer" class="footer">
     <footer>
-        <div class="footertotal">
-            <div class="footerinfo">
-            <h3>Sobre a WARPR</h3>
+        <div class="container line">
             
-            <br> Bacon ipsum dolor amet frankfurter pork chop t-bone, hamburger biltong ball tip shoulder bacon corned beef pastrami cow beef. Ground round t-bone flank pork belly pork chop picanha frankfurter sausage alcatra chicken corned beef porchetta pork.
-            <a href="team.php" class="login"> SAIBA MAIS SOBRE NÓS ></a>
+            <div class="footer_menu">
+                <ul>
+                <li class="footer_info_header">+ informações</li>
+                <li><a href="comofunciona.php" class="login">Como Funciona</a></li>
+                <li><a href="taxas.php" class="login">Taxa e Encargos</a></li>
+                <li><a href="central.php" class="login">Central de Ajuda</a></li>
+                <li><a href="#" class="login">Contate-nos</a></li>
+                </ul>
+            </div>
+        
+            <div class="footer_info">
+                <div class="footer_info_header">Sobre a WARPR</div>
+            
+            <div class="footer_info_text">Hand it help man darling it with sin fools goes darling in sin rush shall my surely but i love stay it falling falling it goes would in so to falling surely take goes meant the fools rush take life so.</div>
+            <a href="team.php"> SAIBA MAIS SOBRE NÓS ></a>
                 <div align="right">
                     <img class="login icon" src="img/icons/svg/deviantart.svg" height="32px">
                     <img class="login icon" src="img/icons/svg/tumblr.svg" height="32px">
@@ -126,19 +131,9 @@ if (isset($_POST['btnLogin'])) {
                 </div>
             </div>
 
-            <div class="footermenu">
-                <ul>
-                <li><h4>+ informações!</h4></li>
-                <li><a href="comofunciona.php" class="login">Como Funciona</a></li>
-                <li><a href="taxas.php" class="login">Taxa e Encargos</a></li>
-                <li><a href="central.php" class="login">Central de Ajuda</a></li>
-                <li><a href="#" class="login">Contate-nos</a></li>
-                </ul>
-            </div>
-            <hr color=#666 id="bottomx">
-
-            <div class="footerfooter">COPYRIGHT © 2016 WARPR - TODOS OS DIREITOS RESERVADOS | <a class="login" href="#">POLÍTICA DE PRIVACIDADE</a> | <a class="login" href="#">TERMOS E CONDIÇÕES</a></div>
+            
         </div>
+        <div class="footer_rights">COPYRIGHT © 2016 WARPR - TODOS OS DIREITOS RESERVADOS | <a class="login" href="#">POLÍTICA DE PRIVACIDADE</a> | <a class="login" href="#">TERMOS E CONDIÇÕES</a></div>
     </footer>
     </div>
 </body>
